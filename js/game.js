a565481ee63e93aaaf8c9246ba3a4d97e647696f
ririso <your-email-address>
2022-03-05 14:22:34 +0100

    Javascript size change bug fix

--- js/game.js
+++ js/game.js
@@ -27,34 +27,35 @@ function randomizePosition(width, height, difference){
     return [xPosition,yPosition];
 }
 function generateBalls(button){
-    var parent = document.getElementById("wrapper");
-    var parentWidth = parent.getBoundingClientRect().width;
-    var parentHeight = parent.getBoundingClientRect().height;
-    var bottom = parent.getBoundingClientRect().bottom;
-    var difference = Math.round(bottom - parentHeight);
+    parent = document.getElementById("wrapper");
+    parentWidth = parent.getBoundingClientRect().width;
+    parentHeight = parent.getBoundingClientRect().height;
+    bottom = parent.getBoundingClientRect().bottom;
+    difference = Math.round(bottom - parentHeight);
     var gameInterval = setInterval(function(){
         if (button.value < "10" ){
             clearInterval(gameInterval);
-        } else{
+        } else{    
             increaseCounterByOne("generatedBalls");
             var ballId = Math.random().toString(16).substr(2, 8);
-            //<input type="button" class="ball" onclick="pop(this)" id="${ballId}"/>
             var ball = document.createElement('input');
             ball.type="button";
             ball.setAttribute("class","ball");
             ball.setAttribute("onclick","pop(this.id, true)");
-            var maxSize = Math.round(50 + Math.random() * 50);
-            var minSize = 20;
+            const maxSize = Math.round(50 + Math.random() * 50);
+            const minSize = 20;
             ball.style.width = maxSize-minSize +"px";
             ball.style.height = maxSize-minSize+"px";
             ball.id = ballId;
-            var positions = randomizePosition(parentWidth, parentHeight, difference);
+            ball.value = ballId;
+            positions = randomizePosition(parentWidth, parentHeight, difference);
             ball.style.left= positions[0];
             ball.style.top= positions[1];
             parent.insertAdjacentElement("beforeend",ball);      
-            changeSize(ballId, button, 20+Math.random()*120, maxSize, minSize);
+            changeSize(ballId, button, maxSize, minSize);
+            //changePosition(ballId, button, parentHeight, parentWidth, difference)
         }
-    },500+Math.random()*100);
+    },700+Math.random()*100);
 }
 function removeLeftoverBalls(){
     var parent = document.getElementsByClassName("ball");
@@ -62,6 +63,46 @@ function removeLeftoverBalls(){
         parent[0].parentNode.removeChild(parent[0]);
     }
 }
+/*function changePosition(ballId, button, parentHeight, parentWidth, difference){
+    const interval = 10;
+    const xCoord = document.getElementById(ballId).style.left;
+    const yCoord = document.getElementById(ballId).style.top;
+    var numberOfChanges = 0;
+    var sizeInterval = setInterval(function(){
+        if (document.getElementById(ballId) != null){
+            if (Math.abs(document.getElementById(ballId).style.left - xCoord) > 100 ){
+                numberOfChanges += 1;
+            }
+            if (numberOfChanges == 9 || button.value == "0"){
+                pop(ballId, false);
+                clearInterval(sizeInterval);
+            }
+            else if (numberOfChanges %2 ==0){
+                moveLeft(ballId);
+            }
+            else if (numberOfChanges%2 != 0){
+                moveRight(ballId);
+            }
+        }
+        else{
+            clearInterval(sizeInterval);
+        }
+    },interval);
+    function moveLeft(ballId){
+        var xCoord = document.getElementById(ballId).style.width;
+        indexOfP = xCoord.indexOf('p');
+        var xCoordNumber = parseInt(xCoord.substring(0,indexOfP))-1;
+        xCoord = xCoordNumber + "px";
+        document.getElementById(ballId).style.left=xCoord;
+    }
+    function moveRight(ballId){
+        var xCoord = document.getElementById(ballId).style.width;
+        indexOfP = xCoord.indexOf('p');
+        var xCoordNumber = parseInt(xCoord.substring(0,indexOfP))+1;
+        xCoord = xCoordNumber + "px";
+        document.getElementById(ballId).style.left=xCoord;
+    }
+}*/
 
 function resetValues(){
     document.getElementById("score").setAttribute("value", "0");
@@ -91,9 +132,10 @@ function pop(ballId, didItScore){
     }
 }
 
-function changeSize(ballId, button, interval, maxSize, minSize){
-    maxSizePixels = maxSize + "px";
-    minSizePixels = minSize + "px";
+function changeSize(ballId, button, maxSize, minSize){
+    const maxSizePixels = maxSize + "px";
+    const minSizePixels = minSize + "px";
+    const interval = 25 + Math.random()*80;
     var numberOfChanges = 0;
     var sizeInterval = setInterval(function(){
         if (document.getElementById(ballId) != null){
@@ -119,25 +161,22 @@ function changeSize(ballId, button, interval, maxSize, minSize){
 
 function increaseSize(ballId){
     if (document.getElementById(ballId) != null){
-        ball = document.getElementById(ballId);
-        var sizeOfBall = ball.style.width;
+        var sizeOfBall = document.getElementById(ballId).style.width;
         indexOfP = sizeOfBall.indexOf('p');
         var sizeNumber = parseInt(sizeOfBall.substring(0,indexOfP))+1;
         sizeOfBall = sizeNumber + "px";
-        ball.style.width = sizeOfBall;
-        ball.style.height = sizeOfBall;
+        document.getElementById(ballId).style.width = sizeOfBall;
+        document.getElementById(ballId).style.height = sizeOfBall;
     }
-
 }
 
 function decreaseSize(ballId){
     if (document.getElementById(ballId) != null){
-        ball = document.getElementById(ballId);
-        var sizeOfBall = ball.style.width;
+        var sizeOfBall = document.getElementById(ballId).style.width;
         indexOfP = sizeOfBall.indexOf('p');
         var sizeNumber = parseInt(sizeOfBall.substring(0,indexOfP))-1;
         sizeOfBall = sizeNumber + "px";
-        ball.style.width = sizeOfBall;
-        ball.style.height = sizeOfBall;
+        document.getElementById(ballId).style.width = sizeOfBall;
+        document.getElementById(ballId).style.height = sizeOfBall;
     }
 }
